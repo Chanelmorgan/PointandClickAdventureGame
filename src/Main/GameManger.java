f6a606eb9c3015b1ed55de26f9fec7deb6d80076
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-11 12:21:56 +0100

    Creating the message text

--- src/Main/GameManger.java
+++ src/Main/GameManger.java
@@ -2,10 +2,13 @@ package Main;
 
 public class GameManger {
 
+    UI ui = new UI(this);
+
     public static void main(String[] args) {
         new GameManger();
     }
 
+    // Constructor
     public GameManger(){
 
     }
--- src/Main/UI.java
+++ src/Main/UI.java
@@ -1,8 +1,42 @@
 package Main;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class UI {
 
-    JFrame window = new JFrame();
+    GameManger gm;
+
+    JFrame window;
+    public JTextArea messageText;
+
+
+    // Constructor
+    public UI(GameManger gm){
+        this.gm = gm;
+
+        createMainField();
+        window.setVisible(true);
+    }
+
+    // Creates the main frame
+    public void createMainField(){
+        window = new JFrame();
+        window.setSize(800, 800);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.getContentPane().setBackground(Color.BLACK);
+        window.setLayout(null);
+
+        // Creating and adding the message text
+        messageText = new JTextArea("This is sample text ");
+        messageText.setBounds(50, 400, 700, 150);
+        messageText.setBackground(Color.BLACK);
+        messageText.setForeground(Color.WHITE);
+        messageText.setEditable(false);
+        messageText.setLineWrap(true);
+        messageText.setWrapStyleWord(true);
+        messageText.setFont(new Font("Book Antiqua", Font.PLAIN, 26)); //not working
+        window.add(messageText);
+
+    }
 }
