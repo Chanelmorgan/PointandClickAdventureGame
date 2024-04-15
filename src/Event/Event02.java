217810285db2a7715fb8a6a35b9cf5f33e38955c
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-15 13:40:49 +0100

    Adding events for scene 2 the text

--- src/Event/Event02.java
+++ src/Event/Event02.java
@@ -11,26 +11,32 @@ public class Event02 {
     }
 
     public void lookCave(){
+        gm.ui.messageText.setText("It's a cave!");
 
     }
 
     public void talkCave(){
+        gm.ui.messageText.setText("YOu hear the echo of your voice");
 
     }
 
     public void enterCave(){
+        gm.ui.messageText.setText("It's too dark to enter");
 
     }
 
     public void lookRoot(){
+        gm.ui.messageText.setText("Thers's something at the tree");
 
     }
 
     public void talkRoot(){
+        gm.ui.messageText.setText("They say plant grows well if you talk to but this tree doesn't look like it needs encouragement");
 
     }
 
     public void searchRoot(){
+        gm.ui.messageText.setText("You find a lantern!");
 
     }
 }
