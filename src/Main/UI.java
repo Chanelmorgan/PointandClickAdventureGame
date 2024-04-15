6c0993d9e16471e211b746c09783b97a72aa3b33
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-15 08:31:08 +0100

    Creating the scene changer class

--- src/Main/UI.java
+++ src/Main/UI.java
@@ -35,7 +35,7 @@ public class UI {
         window.setTitle("Adventure Game");
 
         // Creating and adding the message text
-        messageText = new JTextArea("This is sample text ");
+        messageText = new JTextArea("");
         messageText.setBounds(50, 410, 700, 150);
         messageText.setBackground(Color.BLACK);
         messageText.setForeground(Color.WHITE);
@@ -76,6 +76,7 @@ public class UI {
 
 
 
+
     }
 
     public void createObject(int bgNum, int objx, int objy, int objWidth, int objHeight, String fileName, String choice1Name,
@@ -86,17 +87,17 @@ public class UI {
         JMenuItem menuItem[] = new JMenuItem[4]; // using [1], [2], [3]
         menuItem[1] = new JMenuItem(choice1Name);
         menuItem[1].addActionListener(gm.aHandelr);
-        menuItem[1].setActionCommand("choice1Command");
+        menuItem[1].setActionCommand(choice1Command);
         popMenu.add(menuItem[1]);
 
         menuItem[2] = new JMenuItem(choice2Name);
         menuItem[2].addActionListener(gm.aHandelr);
-        menuItem[2].setActionCommand("choice2Command");
+        menuItem[2].setActionCommand(choice2Command);
         popMenu.add(menuItem[2]);
 
         menuItem[3] = new JMenuItem(choice3Name);
         menuItem[3].addActionListener(gm.aHandelr);
-        menuItem[3].setActionCommand("choice3Command");
+        menuItem[3].setActionCommand(choice3Command);
         popMenu.add(menuItem[3]);
 
         // create objects
@@ -117,6 +118,7 @@ public class UI {
 
         // Set the scaled ImageIcon as the icon of the JLabel
         objectLabel.setIcon(scaledIcon);
+        bgPanel[bgNum].add(objectLabel);
 
         // adding mouse listener to the objects
         objectLabel.addMouseListener(new MouseListener() {
@@ -150,15 +152,48 @@ public class UI {
         });
 
         bgPanel[bgNum].add(objectLabel);
-        bgPanel[bgNum].add(bgLabel[bgNum]);
+
 
 
     }
 
+    public void createArrowButton(int bgNum, int x, int y, int width, int height, String arrowFileName, String command){
+        ImageIcon originalArrowIcon = new ImageIcon(getClass().getClassLoader().getResource(arrowFileName));
+
+        // Resize the original arrow image to fit within the dimensions of the button
+        Image scaledArrowImage = originalArrowIcon.getImage().getScaledInstance(width, height, Image.SCALE_SMOOTH);
+
+        // Create a new ImageIcon from the scaled image
+        ImageIcon scaledArrowIcon = new ImageIcon(scaledArrowImage);
+
+        JButton arrowButton = new JButton();
+        arrowButton.setBounds(x, y, width, height);
+        arrowButton.setBackground(null);
+        arrowButton.setContentAreaFilled(false);
+        arrowButton.setFocusPainted(false);
+        arrowButton.setIcon(scaledArrowIcon);
+        arrowButton.addActionListener(gm.aHandelr);
+        arrowButton.setActionCommand(command);
+        arrowButton.setBorderPainted(false);
+
+        bgPanel[bgNum].add(arrowButton);
+
+    }
+
+    // SCENE 1
     public void generateScreen(){
         createBackground(1, "treeLandscape.png");
         createObject(1, 440, 300, 50, 50, "guard.png", "Look", "Talk", "Attack", "lookGuard", "talkGuard", "attackGuard");
         createObject(1,50, 260, 100, 100, "hut.png", "Look", "Talk", "Rest", "lookHut", "talkHut", "restHut");
         createObject(1,310, 250, 50, 50, "locked-chest.png", "Look", "Talk", "Open", "lookChest", "talkChest", "openChest");
+        createArrowButton(1, 0, 150, 50, 50,"left-arrow.png", "goScene2");
+        bgPanel[1].add(bgLabel[1]);
+
+        // Scene 2
+        createBackground(2, "cave.jpg");
+        //createObject(1, 440, 300, 50, 50, "guard.png", "Look", "Talk", "Attack", "lookGuard", "talkGuard", "attackGuard");
+        createArrowButton(2, 650, 150, 50, 50, "right-arrow.png", "goScrene1");
+        bgPanel[2].add(bgLabel[2]);
+
     }
 }
